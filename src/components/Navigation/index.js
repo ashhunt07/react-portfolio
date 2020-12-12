b322b736c3b13d43af4585834533dfb2b78ff38c
Ashley Hunt <ashhunt07@gmail.com>
2020-12-12 13:48:37 -0600

    trying to get images working

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './style.css';
+// import Logo from '/images/Ashley-H_white.png';
 
 
 export default function Navigation() {
@@ -11,14 +12,14 @@ export default function Navigation() {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand href="#/">
             <img
-            src="/images/Ashley-H_white.png"
+            src="images/Ashley-H_white.png"
             width="auto"
             height="40"
             className="d-inline-block align-top logo pr-3"
             alt="Ashley logo"
           />
           {' '}
-          Ashley Hunt's Porflio
+          Ashley Hunt's Porfolio
         </Navbar.Brand> 
 
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
